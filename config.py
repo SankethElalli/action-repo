95991f1eed69c153aea27e063d77121863649a2d
SankethElalli <122990524+SankethElalli@users.noreply.github.com>
2026-01-28 20:30:15 +0530

    Update MongoDB URI in config.py to use a secure password

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
 import os
-MONGO_URI = os.environ.get('MONGO_URI', 'mongodb+srv://snketh:sankya123@cluster0.9arwp.mongodb.net/Webhook_data')
+MONGO_URI = os.environ.get('MONGO_URI', 'mongodb+srv://snketh:password@cluster0.9arwp.mongodb.net/Webhook_data')
